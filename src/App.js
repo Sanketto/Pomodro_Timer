b32ec9edd313000ef1d93ba4774293fb1c8e1a6a
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-08-11 19:29:29 +0530

    Google OAuth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState, useEffect} from 'react';
+import {GoogleLogin} from '@react-oauth/google'
 function App() {
+
+  const responseMsg = (res)=>{
+    console.log(res);
+  }
+  const errorMsg =(err)=>{
+    console.log(err);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <div>
+         <GoogleLogin onSuccess={responseMsg} onError={errorMsg}/>
+      </div>
   );
+  
 }
 
 export default App;
